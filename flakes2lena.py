ee813f3f135911b5bcea3e9067c85c47307916ce
JJ Bai <jhb342@cornell.edu>
2023-10-15 16:01:19 -0400

    most recent changes: - deleted contour cause it doesn't work yet

--- flakes2lena.py
+++ flakes2lena.py
@@ -14,7 +14,6 @@ grey_scale = color.rgb2gray(io.imread("im_11.bmp"))
 
 pic = cv.imread("im_11.bmp",)
 ret, thresh1 = cv.threshold(pic, 127, 255, cv.THRESH_BINARY_INV)
-contours, hierarchy = cv.findContours(thresh1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 plt.imshow(pic)
 plt.show()
 
