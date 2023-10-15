f18d775a222122b8609464f751734bb6d1b66aea
JJ Bai <jhb342@cornell.edu>
2023-10-15 16:00:12 -0400

    Added: - image reading - image grey scaling - image binarizing

--- flakes2lena.py
+++ flakes2lena.py
@@ -1,3 +1,22 @@
-import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as img 
+from skimage import color
+from skimage import io
+import cv2 as cv
 
-a = cv2.imread("/10X_example_images/im07.bmp") 
+a = img.imread("im_11.bmp") 
+# plt.imshow(a)
+# plt.show()
+
+grey_scale = color.rgb2gray(io.imread("im_11.bmp"))
+# plt.imshow(grey_scale)
+# plt.show()
+
+pic = cv.imread("im_11.bmp",)
+ret, thresh1 = cv.threshold(pic, 127, 255, cv.THRESH_BINARY_INV)
+contours, hierarchy = cv.findContours(thresh1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+plt.imshow(pic)
+plt.show()
+
+
+# Figure 1: outlining flakes
