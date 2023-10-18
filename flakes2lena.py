721f7de7386b48a89445fbdeba27bf79f43056f3
JJ Bai <jhb342@cornell.edu>
2023-10-18 14:39:19 -0400

    Updates: - Completed Figure 1: outlining the image - put im_11.bmp back to the image folder and added path to image reads

--- flakes2lena.py
+++ flakes2lena.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as img 
+import matplotlib.image as img
 from skimage import color
 from skimage import io
 import cv2 as cv
 
-a = img.imread("im_11.bmp") 
+a = img.imread("10X_example_images/im_11.bmp")
 # plt.imshow(a)
 # plt.show()
 
-grey_scale = color.rgb2gray(io.imread("im_11.bmp"))
+grey_scale = color.rgb2gray(io.imread("10X_example_images/im_11.bmp"))
 # plt.imshow(grey_scale)
 # plt.show()
 
-pic = cv.imread("im_11.bmp",)
+pic = cv.imread(
+    "10X_example_images/im_11.bmp",
+)
 ret, thresh1 = cv.threshold(pic, 127, 255, cv.THRESH_BINARY_INV)
 plt.imshow(pic)
 plt.show()
 
 
-# Figure 1: outlining flakes
+# Figure 1: outlining ALL flakes
+edged = cv.Canny(cv.cvtColor(pic, cv.COLOR_BGR2GRAY), 30, 200)
+contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+cv.drawContours(pic, contours, -1, (0, 255, 0), 3)
+plt.imshow(pic)
+plt.show()
+
+# Figure 2: finding centroids of all flake pieces with dust removed
