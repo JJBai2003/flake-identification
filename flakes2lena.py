479a009b0dc34b836bb5f1c1be3aa422416f170c
JJ Bai <jhb342@cornell.edu>
2023-10-18 14:58:57 -0400

    Update: - Started figure 2 generation code: outlines some big flakes but not all.   Might be issue with the outlining/contouring algorithm

--- flakes2lena.py
+++ flakes2lena.py
@@ -16,15 +16,35 @@ pic = cv.imread(
     "10X_example_images/im_11.bmp",
 )
 ret, thresh1 = cv.threshold(pic, 127, 255, cv.THRESH_BINARY_INV)
+
+plt.subplot(2, 2, 1)
 plt.imshow(pic)
-plt.show()
+# plt.show()
 
 
 # Figure 1: outlining ALL flakes
 edged = cv.Canny(cv.cvtColor(pic, cv.COLOR_BGR2GRAY), 30, 200)
 contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 cv.drawContours(pic, contours, -1, (0, 255, 0), 3)
+plt.subplot(2, 2, 2)
 plt.imshow(pic)
-plt.show()
+# plt.show()
 
 # Figure 2: finding centroids of all flake pieces with dust removed
+
+pic = cv.imread(
+    "10X_example_images/im_11.bmp",
+)
+edged = cv.Canny(cv.cvtColor(pic, cv.COLOR_BGR2GRAY), 30, 200)
+contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+areas = {}
+filtered_contours = []
+for i in range(len(contours)):
+    if cv.contourArea(contours[i]) > 1500:
+        areas[i] = cv.contourArea(contours[i])
+        filtered_contours.append(contours[i])
+print(areas)
+cv.drawContours(pic, filtered_contours, -1, (0, 255, 0), 3)
+plt.subplot(2, 2, 3)
+plt.imshow(pic)
+plt.show()
